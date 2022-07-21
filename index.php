19fe67fcfbd0a73ea080e54d8dff6331bad843ef
E-Lop <edmondolopez@gmail.com>
2022-07-21 14:59:11 +0200

    stampato paragrafo e sua lunghezza

--- index.php
+++ index.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <title>Redacted</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+    Stampare a schermo il paragrafo e la sua lunghezza.
+    Una parola da censurare viene passata dall'utente tramite parametro GET.
+    Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+    <?php
+    $longText = "Tre Anelli ai Re degli Elfi sotto il cielo che risplende,
+    Sette ai Principi dei Nani nelle loro rocche di pietra,
+    Nove agli Uomini Mortali che la triste morte attende,
+    Uno per l'Oscuro Sire chiuso nella reggia tetra,<br>
+    Nella Terra di Mordor, dove l'Ombra nera scende.<br>
+    Un Anello per domarli, un Anello per trovarli,<br>
+    Un Anello per ghermirli e nel buio incatenarli,<br>
+    Nella Terra di Mordor, dove l'Ombra cupa scende.";
+    $textLegth = strlen($longText);
+    ?>
+
+    <h2>Testo originale:</h2>
+    <div>Lunghezza paragrafo: <?php echo $textLegth ?> parole</div>
+    <p><?php echo $longText ?></p>
 </body>
 </html>
